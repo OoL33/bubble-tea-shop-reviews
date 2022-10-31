a42e76cf8a19122b2484b143b026b03b08a172b2
katief906 <katief906@gmail.com>
2022-10-31 17:02:28 -0400

    updated indentation formatting

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,7 @@
 class Shop < ApplicationRecord
-    validates :name, presence: true
-    validates :address, presence: true
-    validates :city, presence: true
-    validates :state, presence: true
-    validates :zip, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
 end
