202cffdfedc02ccc37cd5b1fb804dbc77d5dcc41
katief906 <katief906@gmail.com>
2022-11-07 13:43:27 -0500

    removed duplicate methods for authorization/authentication

--- app/controllers/api/v1/shops_controller.rb
+++ app/controllers/api/v1/shops_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ShopsController < ApiController 
   before_action :authenticate_user, except: [:index, :show]
-  before_action :authorize_user, except: [:index, :show, :create]
+  before_action :authorize_user, except: [:create]
 
   def index
     render json: Shop.all
